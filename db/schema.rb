b599e89c82acd8c5083b1e18b3c72d6d9924e28b
Lukasz Strzyzewski <l.j.strzyzewski@gmail.com>
2017-08-23 20:15:21 +0000

    Added create action to save to db

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20170823171846) do
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "text"
+    t.text     "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
