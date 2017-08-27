ae493d5d28048e1fa7d41632ec25f6fc1b85a512
Lukasz Strzyzewski <l.j.strzyzewski@gmail.com>
2017-08-27 12:31:32 +0000

    Added server side code for creating subscriptions for pro users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # Whitelist the following form fields so that we can process them, if coming from
+  # a Devise sign up form.
+  before_action :configure_premitted_parameters, if: :devise_controller?
+  
+  protected
+    def configure_premitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
+    end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   #GET request for / which is our homepage
-    def home
-      @basic_plan = Plan.find(1)
-      @pro_plan = Plan.find(2)
-    end
+  def home
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
+  end
     
     def about
     end
